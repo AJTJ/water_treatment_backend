a2712be18d6331cf12ca316669cc752410d194a1
AJTJ <janke.aaron@gmail.com>
2024-09-22 14:54:23 +0800

    data models updated with full attributes

--- app/models/equipment.py
+++ app/models/equipment.py
@@ -2,17 +2,31 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing import List, Optional
 import uuid
-
 from app.schemas.equipment import EquipmentStatus
 
 
+# TODO Update the database
 class EquipmentBase(BaseModel):
     id: uuid.UUID
     name: str
     description: Optional[str] = None
+
+    # make = honda
+    manufacturer: Optional[str] = None
+    # model = civic
     equipment_model_number: Optional[str] = None
-    location: Optional[str] = None
+    # serial number = bin number of that car
+    serial_number: Optional[str] = None
+
+    in_plant_location: Optional[str] = None
     image_url: Optional[str] = None
+
+    # Supplier information
+    supplier_names: List[str] = []
+
+    # TODO: multi-plant things
+
+    # internal things
     status: EquipmentStatus
     created_at: datetime
     updated_at: datetime
--- app/models/equipment_request.py
+++ app/models/equipment_request.py
@@ -1,16 +1,34 @@
+from re import U
 import uuid
 from pydantic import BaseModel
 from typing import List, Optional
 from datetime import datetime
+from enum import Enum as PyEnum
 
 from app.schemas.equipment_request import EquipmentRequestStatus
 
 
+class UrgencyLevels(PyEnum):
+    urgent = "urgent"
+    not_urgent = "not_urgent"
+
+
+# TODO Update the database
+# Equipment/Part request
 class EquipmentRequestBase(BaseModel):
     id: uuid.UUID
+
+    part_name: str
     description: Optional[str] = None
-    employee_name: Optional[str] = None
+    operator_name: Optional[str] = None
     image_url: Optional[str]
+
+    # part things
+    part_number: Optional[str] = None
+    quantity: Optional[int] = None
+    urgency: UrgencyLevels = UrgencyLevels.not_urgent
+
+    # internal things
     equipment_id: uuid.UUID
     status: EquipmentRequestStatus = EquipmentRequestStatus.ACTIVE
     created_at: datetime
@@ -19,14 +37,14 @@ class EquipmentRequestBase(BaseModel):
 
 class EquipmentRequestCreate(BaseModel):
     description: Optional[str] = None
-    employee_name: Optional[str] = None
+    operator_name: Optional[str] = None
     image_url: Optional[str]
     equipment_id: uuid.UUID
 
 
 class EquipmentRequestUpdate(BaseModel):
     description: Optional[str] = None
-    employee_name: Optional[str] = None
+    operator_name: Optional[str] = None
     image_url: Optional[str]
 
 
